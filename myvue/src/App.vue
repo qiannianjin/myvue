532399d64485ea9b8fb41bffe8f706eaab90812c
qiannianjin <1691686144@qq.com>
2020-03-12 17:53:49 +0800

    第一次

---
+++ myvue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+     <a-button type="primary">Button></a-button>
+    <router-link to="/">home</router-link>
+    <button @click="go()">点我跳转</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    go () {
+      this.$router.push('/home')
+    }
+  }
+}
+
+</script>
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
